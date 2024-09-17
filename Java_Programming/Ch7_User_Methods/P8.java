313c6f56ee27349ea59b322a2134539f5c1e0262
NkanyisoMz <104601010+NkanyisoMzusers.noreply.github.com>
2024-09-17 14:37:19 +0200

    local cable company billing methods added

--- Java_Programming/Ch7_User_Methods/P8.java
+++ Java_Programming/Ch7_User_Methods/P8.java
@@ -10,8 +10,44 @@ so that it uses the following methods to calculate the billing amount:
 public class Main {
     public static void main(String[] args){
 
+        Billing residence = new Billing();
+        Billing business = new Billing();
+        System.out.println(business.businessCustomer(10));
+        System.out.println(business.businessCustomer(11));
+
+        System.out.println(residence.residentialCustomer(10));
+
         
 
     }
     
     }
+
+    
+public class Billing {
+    public double residentialCustomer(int premium){
+        double billProcessingFee = 4.50;
+        double basicServiceFee = 20.50;
+        double premiumChannels = 7.50;
+        double amountDue = 0.0;
+
+        amountDue = billProcessingFee + basicServiceFee + premium * premiumChannels;
+
+        return amountDue;
+    }
+    public double businessCustomer(int premium){
+        double billProcessingFee = 15.00;
+        double basicServiceFee = 75.90;
+        double premiumChannels = 50.00;
+        double additionalFee = 5.00;
+        double amountDue = 0.0;
+
+
+        if(premium > 10)
+            amountDue =billProcessingFee + basicServiceFee + (premium - 10)*additionalFee
+                    + premium * premiumChannels;
+        else
+            amountDue = billProcessingFee + basicServiceFee +premium * premiumChannels;
+        return amountDue;
+    }
+}
