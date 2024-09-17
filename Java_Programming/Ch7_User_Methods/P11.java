a7b2b8115d0a89d1001983637203e7fa5fd31135
NkanyisoMz <104601010+NkanyisoMzusers.noreply.github.com>
2024-09-17 17:12:01 +0200

    Added prime method

--- Java_Programming/Ch7_User_Methods/P11.java
+++ Java_Programming/Ch7_User_Methods/P11.java
@@ -8,7 +8,26 @@ otherwise, it returns false.
 public class Main {
     public static void main(String[] args){
 
+        System.out.println(prime(1));
+        System.out.println(prime(2));
+        System.out.println(prime(3));
+        System.out.println(prime(4));
+        System.out.println(prime(5));
+
         
+
+    }
+
+    public  static boolean prime(int num){
+
+        int square = (int) sqrt(num);
+        for(int i = 1;i <= square; i ++ )
+            for (int j = 1; j <= square; j++)
+                if((i * j) == num)
+                    return false;
+
+        return true;
+
 
     }
     
